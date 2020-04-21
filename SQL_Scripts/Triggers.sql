b6e241d135eaa1905c1f7dc67942eb42810d8698
Jose Juan <josejuan-98@hotmail.com>
2020-04-22 00:18:37 +0200

    v2.0 - Tables + Triggers Added

--- SQL_Scripts/Triggers.sql
+++ SQL_Scripts/Triggers.sql
@@ -1,21 +1,8 @@
-SET @path_load = "/home/goldenfox/Escritorio/test1.csv";
+--SET @path_load = "/home/goldenfox/Escritorio/test1.csv";
 
 --LINES TERMINATED BY '\n'
 -- (id, name, type, owner_id, @datevar, rental_price)
 -- set date_made = STR_TO_DATE(@datevar,'%m/%d/%Y');
-
-
-CREATE TABLE aux_sys (
-	sysId INT PRIMARY KEY,
-	sysType VARCHAR(20),
-	navn VARCHAR(80)
-);
-
-LOAD DATA LOCAL INFILE "/home/goldenfox/Escritorio/test2.csv" INTO TABLE test.aux_sys
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY ';'
-IGNORE 2 LINES
-(sysType, sysID, navn);
 
 CREATE TABLE andre_elem (
 	andId INT(11) AUTO_INCREMENT PRIMARY KEY,
@@ -30,40 +17,24 @@ LINES TERMINATED BY '\n'
 IGNORE 2 LINES
 (andId, type, name);
 
-
-DELIMITER //
-CREATE TRIGGER elements_ins AFTER INSERT ON RawData
-  FOR EACH ROW
-    BEGIN
-      IF ((SELECT 1 FROM post WHERE userId = OLD.userId LIMIT 1) <> 1) THEN
-        DELETE FROM user_p WHERE userId = OLD.userId;
-      END IF;
-    END//
-DELIMITER ;
-
-INSERT INTO andre_elem (andId, type, name)
-VALUES('works', systemtype ,'test1');
-
-Use test;
-
 CREATE TABLE Element (
 	ID VARCHAR(255) PRIMARY KEY,
 	TYPE VARCHAR(255),
-	NAME VARCHAR(255)
+	NAME VARCHAR(255),
+	DOCUMENTATION TEXT
 );
 
 
 DELIMITER //
-
-CREATE TRIGGER trig_andre_elem AFTER INSERT
+CREATE TRIGGER trig_elem AFTER INSERT
 ON RawData FOR EACH ROW
 BEGIN
-INSERT INTO andre_elem (type, name)
-VALUES( systemtype ,'test1');
+set @newID = CONCAT( 'SYS_', SUBSTR(NEW.systemtype,1, 3),'_',  CONVERT(LPAD(NEW.system_id,5,0), CHAR) );
+
+INSERT INTO Element (ID, TYPE, NAME, DOCUMENTATION)
+VALUES( @newID, 'ApplicationComponent', NEW.navn, NEW.beskrivelse);
  	END;//
-
 DELIMITER ;
-
 
 LOAD DATA LOCAL INFILE "/home/goldenfox/Escritorio/RawData.csv" REPLACE INTO TABLE RawData
 FIELDS TERMINATED BY ','
@@ -72,10 +43,30 @@ LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (systemtype, system_id, navn, beskrivelse,systemeier,  systemkoordinator, admsone, sikker_sone, elevnett,tu_nett , internettviktighet, personopplysninger,  sensitive_personopplysninger);
 
+select count(*) from RawData;
+drop trigger trig_andre_elem;
 
+CREATE TABLE Relation (
+	ID VARCHAR(255) PRIMARY KEY, --"Constr0001_SYS_Fag_00290" andre_elem.name_Element.ID
+	TYPE VARCHAR(255), --"AssociationRelationship"
+	NAME VARCHAR(255),
+	DOCUMENTATION TEXT,
+	SOURCE INT(11)
+	TARGET VARCHAR(255),
+	FOREIGN KEY TARGET REFERENCES Element (ID)
+	ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY SOURCE REFERENCES andre_element (ID)
+	ON UPDATE CASCADE ON DELETE CASCADE
+);
 
-
-
+CREATE TABLE Property (
+	ID VARCHAR(255),
+	KEY VARCHAR(255),
+	VALUE VARCHAR(255),
+	PRIMARY KEY (ID, KEY),
+	FOREIGN KEY ID REFERENCES Element (ID)
+	ON UPDATE CASCADE ON DELETE CASCADE
+);
 
 
 
