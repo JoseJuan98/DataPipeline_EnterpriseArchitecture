54a164636fb93f2ab9a2573856181021c96e6247
h571552 <31279125+h571552@users.noreply.github.com>
2020-05-11 10:01:23 +0200

    Update db2csv.py

--- database2csv/db2csv.py
+++ database2csv/db2csv.py
@@ -8,38 +8,48 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import mapper, sessionmaker, relationship
 import csv
 
-DB_URL = 'mysql+mysqldb://aaa@testing1238:Abc12345@testing1238.mysql.database.azure.com:3306/mydatabase'
+#DB_URL = 'mysql+mysqldb://aaa@testing1238:Abc12345@testing1238.mysql.database.azure.com:3306/mydatabase'
+DB_URL = 'mysql+mysqldb://dani:Dani1234!@localhost/test'
 ENGINE = create_engine(DB_URL)
 
 Base = declarative_base()
 
 
 class Elements(Base):
-    __tablename__ = "elements"
+    __tablename__ = "Element"
 
-    id = Column('ID', String, primary_key=True)
-    type = Column('Type', String)
-    name = Column('Name', String)
-    documentation = Column('Documentation', String)
+    idEl = Column('idEl', Integer, primary_key=True)
+    id = Column('ID', String)
+    type = Column('TYPE', String)
+    name = Column('NAME', String)
+    documentation = Column('DOCUMENTATION', String)
 
+class Andre_Elements(Base):
+    __tablename__ = "Andre_Elements"
+
+    id = Column('id', Integer, primary_key=True)
+    sid = Column('sid', String)
+    type = Column('type', String)
+    name = Column('name', String)
+    documentation = Column('documentation', String)
 
 class Properties(Base):
-    __tablename__ = "properties"
+    __tablename__ = "Property"
 
     id = Column('ID', String, primary_key=True)
-    key = Column('Key', String)
-    value = Column('Value', String)
+    key = Column('KEY_P', String)
+    value = Column('VALUE_P', String)
 
 
 class Relations(Base):
-    __tablename__ = "relations"
+    __tablename__ = "Relation"
 
     id = Column('ID', String, primary_key=True)
-    type = Column('Type', String)
-    name = Column('Name', String)
-    documentation = Column('Documentation', String)
-    source = Column('Source', String)
-    target = Column('Target', String)
+    type = Column('TYPE', String)
+    name = Column('NAME', String)
+    documentation = Column('DOCUMENTATION', String)
+    source = Column('SOURCE', String)
+    target = Column('TARGET', String)
 
 
 Session = sessionmaker(bind=ENGINE)
@@ -50,30 +60,34 @@ session = Session()
  #result = cursor.execute(query)
 
 elements = session.query(Elements).all()
+andre_elements = session.query(Andre_Elements).all()
 
-with open('atest_elements.csv', 'w', newline='') as file:
+with open('atest_elements.csv', 'w') as file:
     writer = csv.writer(file)
     writer.writerow(["ID", "Type", "Name", "Documentation"])
     for e in elements:
         writer.writerow([e.id, e.type, e.name, e.documentation])
+    for a in andre_elements:
+        writer.writerow([a.sid, e.type, a.name, e.documentation])
 
 
-properties = session.query(Properties).all()
 
-with open('atest_properties.csv', 'w', newline='') as file:
+properties = session.query(Properties, Elements).filter(Properties.id == Elements.idEl).all()
+
+with open('atest_properties.csv', 'w') as file:
     writer = csv.writer(file)
     writer.writerow(["ID", "Key", "Value"])
-    for p in properties:
-        writer.writerow([p.id, p.key, p.value])
+    for p, e in properties:
+        writer.writerow([e.id, p.key, p.value])
 
 
-relations = session.query(Relations).all()
+relations = session.query(Relations, Elements, Andre_Elements).filter(Relations.target == Elements.idEl).filter(Relations.source == Andre_Elements.id).all()
 
-with open('atest_relations.csv', 'w', newline='') as file:
+with open('atest_relations.csv', 'w') as file:
     writer = csv.writer(file)
     writer.writerow(["ID", "Type", "Name", "Documentation", "Source", "Target"])
-    for e in relations:
-        writer.writerow([e.id, e.type, e.name, e.documentation, e.source, e.target])
+    for r, e, a in relations:
+        writer.writerow([r.id, r.type, r.name, r.documentation, a.sid, e.id])
 
 
 #ID Type Name Documentation - elements
