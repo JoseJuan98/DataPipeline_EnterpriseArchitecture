8244d1bf1293f3444df2d783373713b911db3318
h571552 <31279125+h571552@users.noreply.github.com>
2020-05-19 17:04:02 +0200

    Temporary version
    Temporary version to work with cron as it does not support relative paths

--- SQL_Scripts/database2csv/gitcsvscriptpull.sh
+++ SQL_Scripts/database2csv/gitcsvscriptpull.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-#This library is needed for the script
 
+P="/home/dani/WebstormProjects/Project_D24_Bergen-Kommune/SQL_Scripts/database2csv"
 USER=$1
 LREPO=$2
-REPO="https://github.com/$USER/$LREPO.git"
-if [ -d "$LREPO" ]; then
+REPO="git@github.com/$USER/$LREPO.git"
+if [ -d "$P/$LREPO" ]; then
   echo "Directory exists, pulling"
   git pull $LREPO master --allow-unrelated-histories
 else
   git clone $REPO
   git remote add $LREPO $REPO
 fi
+
