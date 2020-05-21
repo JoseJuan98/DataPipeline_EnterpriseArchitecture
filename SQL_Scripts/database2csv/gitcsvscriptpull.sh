1d8b0bf38e1d713180908fb536a1ce26199bc9bc
h571552 <31279125+h571552@users.noreply.github.com>
2020-05-21 16:21:13 +0200

    Should work with crontab now
    (replace the password)

--- SQL_Scripts/database2csv/gitcsvscriptpull.sh
+++ SQL_Scripts/database2csv/gitcsvscriptpull.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-P="/home/dani/WebstormProjects/Project_D24_Bergen-Kommune/SQL_Scripts/database2csv"
+cd /home/dani/WebstormProjects/Project_D24_Bergen-Kommune/SQL_Scripts/database2csv
+
 USER=$1
 LREPO=$2
-REPO="git@github.com/$USER/$LREPO.git"
-if [ -d "$P/$LREPO" ]; then
+REPO="https://github.com/$USER/$LREPO.git"
+if [ -d "$LREPO" ]; then
   echo "Directory exists, pulling"
   git pull $LREPO master --allow-unrelated-histories
 else
   git clone $REPO
-  git remote add $LREPO $REPO
+  #git remote add $LREPO $REPO
+  git remote add "https://$USER:password@github.com/$USER/$LREPO.git"
 fi
-
