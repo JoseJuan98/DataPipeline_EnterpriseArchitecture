3e0b14dd32512d30bb50c90adffaa5144a35d465
JoseJuan98 <josejuan-98@hotmail.com>
2020-05-27 23:38:42 +0200

    ArchiModel 3.2

--- csv2mysql/instScript.sh
+++ csv2mysql/instScript.sh
@@ -2,5 +2,5 @@
 #This library is needed for the script
 
 #MySQL Database (client, server), python, python-pip.
-sudo apt-get install python3.7-mysqldb
+apt-get install python3.7-mysqldb
 pip3 install mysql-connector-python
